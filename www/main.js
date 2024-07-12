7fde903e33dab551e92af232116d7db79ff7944e
Deep Sarkar <144143057+sk1ees@users.noreply.github.com>
2024-07-12 18:07:05 +0530

    frontend-assistant-done

---
+++ www/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+  $(".text").textillate({
+    loop: true,
+    sync: true,
+    in: {
+      effect: "bounceIn",
+    },
+    out: {
+      effect: "bounceOut",
+    },
+  });
+});
