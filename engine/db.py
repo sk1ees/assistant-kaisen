2ee4375cc628b3363928e62c903e08c1b0d6f3d4
Deep Sarkar <144143057+sk1ees@users.noreply.github.com>
2024-07-13 15:11:10 +0530

    added retrieve logic from the database

--- engine/db.py
+++ engine/db.py
@@ -8,12 +8,15 @@ query = "CREATE TABLE IF NOT EXISTS sys_commands(id integer primary key , name V
 cursor.execute(query)
 
 
-# query = "INSERT INTO sys_commands VALUES (null,'githubDesktop','C:\\Users\\dashi\\AppData\\Local\\GitHubDesktop\\GitHubDesktop.exe')"
+# query = "INSERT INTO sys_commands VALUES (null,'audacity','C:\\Program Files\\Audacity\\Audacity.exe')"
 # cursor.execute(query)
 # conn.commit()
 
 query = "CREATE TABLE IF NOT EXISTS web_commands(id integer primary key,name VARCHAR(100),url VARCHAR(1000))"
 cursor.execute(query)
+
+# cursor.execute("DELETE FROM sys_commands WHERE id = 1") deleted one data from the database
+# conn.commit()
 
 # query = "INSERT INTO web_commands VALUES (null,'youtube','https://www.youtube.com/')"
 # cursor.execute(query)
--- engine/features.py
+++ engine/features.py
@@ -1,12 +1,16 @@
 import os
 import re
+import sqlite3
+import webbrowser
 from playsound import playsound
 import eel
 import pywhatkit as pwt
 from engine.command import speak
 from engine.config import ASSISTANT_NAME
 # playAssistantSound function 
+conn = sqlite3.connect("assistant.db")
 
+cursor = conn.cursor()
 @eel.expose
 def playAssistantSound():
     music_dir = "www\\assets\\audio\\start_sound.mp3"
@@ -18,11 +22,36 @@ def openCommand(query):
     query = query.replace("can you please open", "")
     query.lower()
 
-    if query!="":
-        speak("Wait a second...Opening" + query)
-        os.system('start' +query)
-    else:
-        speak("not found")
+    app_name = query.strip()
+    if app_name!="":
+        try:
+            cursor.execute(
+                'SELECT path FROM sys_commands WHERE name IN (?)',(app_name,))
+            results = cursor.fetchall()
+
+            if len(results)!=0: 
+                speak('Opening ' + query)
+                os.startfile(results[0][0])
+
+            elif len(results)==0:
+                cursor.execute(
+                    'SELECT url FROM web_commands WHERE name IN (?)',(app_name,))
+                results = cursor.fetchall()
+                
+                if len(results)!=0:
+                    speak('Opening ' + query)
+                    webbrowser.open(results[0][0])
+
+                else:
+                    speak("Opening" +query)
+                    try:
+                        os.system('start' + query)
+                    except:
+                        speak("not found")
+        except:
+            speak("something went wrong")
+
+            
 
 def playYoutube(query):
     search_term = extract_yt_term(query)
