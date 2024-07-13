8f7ee9db8fdbd856f12686fb9e7b132c73dc2c3b
Deep Sarkar <144143057+sk1ees@users.noreply.github.com>
2024-07-13 10:56:24 +0530

    updated commands

--- engine/command.py
+++ engine/command.py
@@ -1,12 +1,41 @@
 import pyttsx3
-
+import speech_recognition as sr
+# initializing engine 
 engine = pyttsx3.init('sapi5')
 voices = engine.getProperty('voices') 
 engine.setProperty('voice', voices[1].id)
 engine.setProperty('rate', 174)     
 
-def speak(text):
-    engine.say(text)
+# text to speech 
+def speak(audio):
+    engine.say(audio)
+    print(audio)
     engine.runAndWait()
 
-speak("Welcome Sir! How can i help you?")
+# converting text to speech 
+def takecommand():
+    r= sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Listening...")
+        r.pause_threshold = 1
+        r.adjust_for_ambient_noise(source)
+        audio = r.listen(source, timeout=5 , phrase_time_limit=5)
+
+    try:
+        print("Recognizing...")
+        query  =r.recognize_google(audio , language = 'en-in')
+        speak(f"{query}")
+
+    except Exception as e:
+        speak("you haven't said a word..please talk!")
+        return "none"
+    return query
+
+
+
+if __name__ == '__main__':
+    while True:
+        text = takecommand().lower()
+        if "you can stop now" in text:
+            speak('Thank you sir, and have a great day!')
+            break
